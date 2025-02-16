a0a132f7ebdaafc2f7ab907b1f9e38d6e7ef5b6a
Srini Syave <srini.syave@rotary.org>
2025-02-17 00:34:17 +0530

    added new files

---
+++ src/app/home/order-page/order-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-page',
+  imports: [],
+  templateUrl: './order-page.component.html',
+  styleUrl: './order-page.component.css'
+})
+export class OrderPageComponent {
+
+}
---
+++ src/app/home/product-home/product-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-home',
+  imports: [],
+  templateUrl: './product-home.component.html',
+  styleUrl: './product-home.component.css'
+})
+export class ProductHomeComponent {
+
+}
